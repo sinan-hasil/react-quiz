f3cde5a1e87f74d2bfe67a57d53f57d374acc75b
sinan-hasil <sinan.hasil61@gmail.com>
2024-08-20 10:32:02 +0300

    navbar hazır

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,9 +1,26 @@
-
+import { Button, Container, Nav } from "react-bootstrap"
+import { Link, Outlet } from "react-router-dom"
+import logo from "./image/39202732-6c1ba82e-4810-11e8-81e7-4e8ad89110ba.png"
 
 const Root = () => {
   return (
     <>
-        
+        <nav>
+            <Container className="nav-container">
+                <div className="logo">
+                    <Nav.Link as={Link} to={"/"}>
+                        <img src={logo} />
+                    </Nav.Link>
+                </div>
+
+                <div className="nav-btn">
+                    <Nav.Link as={Link} to={""}>
+                        <Button variant="danger">Giriş Yap</Button>
+                    </Nav.Link>
+                </div>
+            </Container>
+        </nav>
+        <Outlet />
     </>
   )
 }
